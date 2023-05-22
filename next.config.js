e84cdf82e83b6b51354a889cc2e53e0d800b3d47
zuidap <1179659321@qq.com>
2023-05-22 11:24:32 +0800

    更新配置next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
 /** @format */
 
-const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? '' : undefined,
-  basePath: '',
-  //编译图标
-  devIndicators: {
-    buildActivityPosition: 'bottom-right',
-    buildActivity: true,
-  },
-  //distDir build输出目录
-  distDir: 'build',
-  //信任域名，否则Image不显示
-  experimental: {
-    urlImports: ['https://example.com/assets/'],
-  },
-  //导出静态
-  // output: "export",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
